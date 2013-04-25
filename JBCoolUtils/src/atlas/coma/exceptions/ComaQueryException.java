3c5c3474c6cb777349f9a1c4105271577cdfcb26
Andrea Formica <andrea.formica@cern.ch>
2013-04-25 10:20:02 +0200

    Add Exception class for COMA.

---
+++ JBCoolUtils/src/atlas/coma/exceptions/ComaQueryException.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package atlas.coma.exceptions;
+
+/**
+ * @author formica
+ *
+ */
+public class ComaQueryException extends Exception {
+
+	/**
+	 * 
+	 */
+	public ComaQueryException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public ComaQueryException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public ComaQueryException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public ComaQueryException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
