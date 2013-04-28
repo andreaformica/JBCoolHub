900f8bbb1ed0e8c3be2acd47f9401ff887c05225
Andrea Formica <andrea.formica@cern.ch>
2013-04-29 00:03:15 +0200

    Add package info for javadoc.

---
+++ JBCoolEjbs/ejbModule/atlas/cool/rest/model/package-info.java
@@ -0,0 +1,19 @@
+package atlas.cool.rest.model;
+
+/**
+ * <HTML> <BODY>
+ * <p>
+ * This package contains definitions of POJOs which are used to retrieve types returned by Oracle
+ * PL/SQL package cool_select_pkg inside the account ATLAS_COND_TOOLS. 
+ * </p>
+ * <p>
+ * Every POJO contains annotations with the queries that are used for retrieval inside the DAOs.
+ * The queries are named, and in order to provide arguments to the query, an annotation CoolQuery
+ * has been used, where the argument names are listed.
+ * <p>
+ * </BODY>
+ * </HTML>
+ * 
+ * @author formica
+ * 
+**/
