bdb18b71edc345a6e93b63e7695ccd3058cb6d98
Andrea Formica <andrea.formica@cern.ch>
2013-05-08 15:53:14 +0200

    Add package-info.

---
+++ JBRestCool/src/atlas/cool/rest/web/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * <HTML> <BODY>
+ * <p>
+ * This package contains definitions of REST services which are used to retrieve types returned by Oracle
+ * PL/SQL package cool_select_pkg inside the account ATLAS_COND_TOOLS. 
+ * </p>
+ * <p>
+ * The services have an API which is documented in every class.
+ * <p>
+ * </BODY>
+ * </HTML>
+ * 
+ * @author formica
+ * 
+**/
+package atlas.cool.rest.web;
+
